86b675232f3806d1f9f16adc7b85d8d5f67928e1
Gardimy Charles <gardimycharles889@gmail.com>
2023-04-10 16:23:15 -0400

    Fix eslint errors

--- hamberger.js
+++ hamberger.js
@@ -1,13 +1,7 @@
 const hamberger = document.querySelector('.Hamberger');
 const Mobilemenu = document.querySelector('.mobile-menu');
-const Closebtn = document.querySelector('.Closebtn');
 
-
-hamberger.addEventListener('click', function () {
-	hamberger.classList.toggle('is-active');
-	Mobilemenu.classList.toggle('is-active');
-});
-
-Closebtn.addEventListener('click', function() {
-	option.style.top = '100%';
+hamberger.addEventListener('click', () => {
+  hamberger.classList.toggle('is-active');
+  Mobilemenu.classList.toggle('is-active');
 });
