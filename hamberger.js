988a13c1989eaff4299af07193ebc41d72c45434
Gardimy Charles <gardimycharles889@gmail.com>
2023-04-11 11:00:38 -0400

    Fix all errors

--- hamberger.js
+++ hamberger.js
@@ -2,12 +2,12 @@ const option = document.querySelector('.option');
 const menu = document.querySelector('.hamberger');
 const closemenu = document.querySelector('.Closebtn');
 
-menu.addEventListener('click', function(){
+menu.addEventListener('click', () => {
   option.style.display = 'flex';
-  menu.style.display ='none';
+  menu.style.display = 'none';
 });
 
-closemenu.addEventListener('click', function(){
+closemenu.addEventListener('click', () => {
   option.style.top = '100%';
-  closemenu.style.display ='block';
+  closemenu.style.display = 'block';
 });
