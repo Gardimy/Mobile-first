e6c5da1c1d7293788ed22631f7885e07aed43a39
mrcbq <marcobaquero@gmail.com>
2023-04-14 11:53:57 -0500

    create the validation function

--- form.js
+++ form.js
@@ -1,5 +1,7 @@
 const form = document.querySelector('form');
 const email = document.getElementById('email');
+const error = email.nextElementSibling;
+const emailRegExp = /^[a-z-0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z-0-9-]+(?:\.[a-z-0-9-]+)*$/;
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
@@ -9,4 +11,10 @@ form.addEventListener('submit', (event) => {
     error.textContent = 'Vour email must not contain capital letters';
     error.className = 'error active';
   } 
+  else {
+    email.className = 'valid';
+    error.textContent = '';
+    error.className = 'error';
+    form.submit();
+  }
 });
