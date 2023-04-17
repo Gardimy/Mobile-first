ebf40c198c236cb0659ecc9888bc3c692e1f118d
Gardimy Charles <gardimycharles889@gmail.com>
2023-04-17 15:28:24 -0400

    Fix Close button

--- Hamberger.js
+++ Hamberger.js
@@ -6,8 +6,3 @@ menu.addEventListener('click', () => {
   option.style.display = 'flex';
   menu.style.display = 'block';
 });
-
-closemenu.addEventListener('click', () => {
-  option.style.top = '-100%';
-  closemenu.style.display = 'none';
-});
