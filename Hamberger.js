bd7020cee5ea7a85cdc01a1160770d0955a7b3c4
Gardimy Charles <gardimycharles889@gmail.com>
2023-04-17 19:41:08 -0400

    add all file for the project

--- Hamberger.js
+++
@@ -1,8 +0,0 @@
-const option = document.querySelector('.option');
-const menu = document.querySelector('.hamberger');
-const closemenu = document.querySelector('.Closebtn');
-
-menu.addEventListener('click', () => {
-  option.style.display = 'flex';
-  menu.style.display = 'block';
-});
