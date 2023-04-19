3240934fba75adec9c404dc2b49bdb6f813b088c
Gardimy Charles <gardimycharles889@gmail.com>
2023-04-19 11:07:44 -0400

    Fix eslint error

--- form.js
+++ form.js
@@ -1,14 +1,20 @@
 const form = document.querySelector('form');
 const mail = document.getElementById('mail');
+const emailRegExp =/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 const warning = mail.nextElementSibling;
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-
+ 
   const isValid = mail.value.length === 0 || emailRegExp.test(mail.value);
   if (!isValid) {
     mail.className = 'invalid';
     warning.textContent = 'Please enter a valid email address" (The email must not contain capital letters!!!)';
     warning.className = 'warning active';
-  } 
+  }else {
+    mail.className = 'valid';
+    warning.textContent = '';
+    warning.className = 'warning';
+    form.submit();
+  }
 });
